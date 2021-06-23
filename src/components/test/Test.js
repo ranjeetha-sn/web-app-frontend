894531def9025d1dbf51a9ca5d6d4e771a9dacd5
ranjeetha-sn <ranjitha.sheel@gmail.com>
2021-06-23 13:07:46 +0530

    made a new folder test in dev branch

---
+++ src/components/test/Test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Test extends Component {
+    render() {
+        return (
+            <div>
+                Hi I am test
+            </div>
+        )
+    }
+}
+export default Test;
